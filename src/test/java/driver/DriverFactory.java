f98f0ea7fc3eaef43b10e4e7fff78906a22b65db
freshman040 <intersoft123@gmx.de>
2026-02-16 16:37:21 +0100

    Tippfehler korrigiert

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -37,7 +37,7 @@ public class DriverFactory {
     private static void applyHeadless(ChromeOptions options){
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usager");
+        options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
     }
 }
